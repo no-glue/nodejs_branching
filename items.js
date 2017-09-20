40f33a67cfd150f5806b6943a5c03cb5b9797a2d
npavlicevic <nikolapav1985@gmail.com>
2017-09-20 03:29:45 +0200

    items

--- items.js
+++ items.js
@@ -1,11 +1,25 @@
+var FAIL_ARR=-1;
 var ListItem = function(){
-  var id,question,options,options_arr,next,next_arr;
+  var id,question,options,optionsArr,next,nextArr;
 }
 var listItemSet = function(item,id,question,options,next){
   item.id=id;
   item.question=question;
   item.options=options;
-  item.options_arr=options.split(",");
+  item.optionsArr=options.split(",");
   item.next=next;
-  item.next_arr=next.split(",");
+  item.nextArr=next.split(",");
+}
+var listNext=function(list,id,option){
+  var item,i,len,opt,next;
+  item=list[id];
+  opt=item.optionsArr;
+  next=item.nextArr;
+  len=opt.length;
+  for(i=0;i<;i++){
+    if(opt[i]==option){
+      return parseInt(next[i]);
+    }
+  }
+  return FAIL_ARR;
 }
