21de52e3b1ae0d489df0227833a366badd6d99b7
npavlicevic <nikolapav1985@gmail.com>
2017-09-20 03:37:29 +0200

    items

--- items.js
+++ items.js
@@ -23,3 +23,15 @@ var listNext=function(list,id,option){
   }
   return FAIL_ARR;
 }
+var listMake=function(config){
+  var len,arr,i,item,conf;
+  len=config.length;
+  arr=new Array(len);
+  for(i=0;i<len;i++){
+    item=new ListItem();
+    conf=config[i];
+    listItemSet(item,conf.id,conf.question,conf.option,conf.next);
+    arr[i]=item;
+  }
+  return arr;
+}
