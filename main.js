6a67eec758cf9dcf67e5406f74d3920eb77647a8
npavlicevic <nikolapav1985@gmail.com>
2017-09-21 01:13:38 +0200

    items

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
-var items=require("./items.js");
+var items=require("./items.js"),
+stdin=process.stdin,
+stdout=process.stdout,
+chunks=[];
+
+stdin.resume();
+stdin.setEncoding('utf8');
+
+stdin.on('data',function(chunk){
+  chunks.push(chunk);
+});
+stdin.on('end',function(){
+});
