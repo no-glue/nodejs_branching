a98292a9a16eeaf21724d9115e262c72e62a64b7
npavlicevic <nikolapav1985@gmail.com>
2017-09-21 02:14:37 +0200

    items

--- main.js
+++ main.js
@@ -17,6 +17,8 @@ stdin.on('end',function(){
   config=JSON.parse(json);
   list=items.listMake(config);
   assert.equal(2,items.listNext(list,1,"nothing"),"item 1 not correct");
+  assert.equal(3,items.listNext(list,2,"m0"),"item 2 not correct m0");
+  assert.equal(items.FAIL_ARR,items.listNext(list,2,"0"),"item 2 not correct 0");
   stdout.write(util.format("%s \n",JSON.stringify(config[0])));
   stdout.write(util.format("%s \n",JSON.stringify(list[1])));
 });
